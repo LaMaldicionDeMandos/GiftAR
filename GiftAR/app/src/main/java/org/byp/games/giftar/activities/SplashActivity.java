1b2c0374f6ab01dd84981186330632ca737c3bb6
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-09-24 22:22:31 -0300

    modifico splash para que sea full screen

---
+++ GiftAR/app/src/main/java/org/byp/games/giftar/activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package org.byp.games.giftar.activities;
+
+import android.os.Bundle;
+
+import org.byp.games.giftar.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+/**
+ * Created by boot on 9/24/15.
+ */
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends RoboActivity {
+    @Override
+    protected void onCreate(Bundle savedInterfaceState) {
+        super.onCreate(savedInterfaceState);
+    }
+}
