e225d211dcd511f8ed3bee5e32fd3b1d3d1bedc0
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-02 23:13:30 -0300

    Al ingresar por primera vez existen dos posibilidades, el usuario no existe, o el usuario existe en estado pending. en el primero se debe crear el usuario en firebase, en el segundo solo hay que poner en estado pending

--- GiftAR/app/src/main/java/org/byp/games/giftar/services/Services.java
+++ GiftAR/app/src/main/java/org/byp/games/giftar/services/Services.java
@@ -1,16 +1,20 @@
 package org.byp.games.giftar.services;
 
+import android.util.Log;
+
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-
-import org.json.JSONObject;
 
 /**
  * Created by boot on 10/2/15.
  */
 @Singleton
 public class Services {
+    private final static String TAG = Services.class.getSimpleName();
     private final Firebase firebase;
 
     @Inject
@@ -20,6 +24,17 @@ public class Services {
 
     public void signUp(final String account) {
         String user = account.replaceAll("\\.","");
-        firebase.child(user).setValue("USER");
+        firebase.child(user).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "Data changed " + dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d(TAG, "Data canceled " + firebaseError);
+            }
+        });
+        //firebase.child(user).setValue("USER");
     }
 }
