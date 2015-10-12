fbdfbbe6382852eb723b316364ed2616ed025198
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-12 01:37:51 -0300

    termino de stylear conctats

--- GiftAR/app/src/main/java/org/byp/games/giftar/activities/MainActivity.java
+++ GiftAR/app/src/main/java/org/byp/games/giftar/activities/MainActivity.java
@@ -1,9 +1,10 @@
 package org.byp.games.giftar.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,26 +15,37 @@ import org.byp.games.giftar.R;
 
 import java.util.List;
 
+import roboguice.activity.RoboActionBarActivity;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-public class MainActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_main)
+public class MainActivity extends RoboActionBarActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    @InjectView(R.id.contacts)
     private RecyclerView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listView = (RecyclerView)findViewById(R.id.contacts);
+        setupToolbar();
         Log.d(TAG, "Agregando contactos");
         List<String> contacts = newArrayList("Aída", "Marcelo");
         RecyclerView.Adapter<ContactsAdapter.ViewHolder> adapter = new ContactsAdapter(contacts);
         listView.setLayoutManager(new LinearLayoutManager(this));
         listView.setAdapter(adapter);
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_gift_32);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
